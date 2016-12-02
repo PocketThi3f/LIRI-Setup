bc5686ff976cf0e4aad012e26721f5137ada43bd
PocketThi3f <krslai@yahoo.com>
2016-12-02 01:04:21 -0800

    powerhousing

--- keys.js
+++ keys.js
@@ -1,8 +1,9 @@
+// Twitter Keys for Callback Function
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
+  consumer_key: 'oYVnXKfnl7ehLTBbKmBG3GhJ0',
   consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  access_token_key: '1273462454-FbKFRLjJt94DMcxCD6aKxslTKYyvuQ1IvHSf2tz',
+  access_token_secret: 'Wcb5QOq1NIHUB7Nq8BdBdwgVOPVfxCYHKUApy6ExrDNQU',
 }
